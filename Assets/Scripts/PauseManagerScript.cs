4aef793567ab2ea163025a2fb5dcea2502226ced
friikjones <d.mullerjones@gmail.com>
2020-09-14 20:21:12 -0700

    First Commit

---
+++ Assets/Scripts/PauseManagerScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManagerScript : MonoBehaviour {
+
+    public GameObject pauseCanvas;
+    public bool paused;
+
+    void Start() {
+        // GameObject.Find("Canvas").GetComponent<GameObject>();
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            paused = !paused;
+        }
+        if (paused) {
+            pauseCanvas.SetActive(true);
+        } else {
+            pauseCanvas.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/SceneTransitionScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitionScript : MonoBehaviour {
+
+    public void SceneToInitialMenu() {
+        SceneManager.LoadScene("InitialMenu", LoadSceneMode.Single);
+    }
+    public void SceneToOptionsMenu() {
+        SceneManager.LoadScene("OptionsMenu", LoadSceneMode.Single);
+    }
+    public void SceneToLevel1() {
+        SceneManager.LoadScene("Level1", LoadSceneMode.Single);
+    }
+
+    public void SceneToQuit() {
+        Application.Quit();
+    }
+
+}
